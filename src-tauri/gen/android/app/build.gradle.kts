b05117478efe17b61d10ab5a3511c8560a675ef9
satouchida <120418469+satouchida@users.noreply.github.com>
2026-04-25 00:01:31 -0400

    fix: bump android compileSdk to 35

--- src-tauri/gen/android/app/build.gradle.kts
+++ src-tauri/gen/android/app/build.gradle.kts
@@ -14,13 +14,13 @@ val tauriProperties = Properties().apply {
 }
 
 android {
-    compileSdk = 34
+    compileSdk = 35
     namespace = "me.satouchida.poweranalyses"
     defaultConfig {
         manifestPlaceholders["usesCleartextTraffic"] = "false"
         applicationId = "me.satouchida.poweranalyses"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = tauriProperties.getProperty("tauri.android.versionCode", "1").toInt()
         versionName = tauriProperties.getProperty("tauri.android.versionName", "1.0")
     }
